c0b1bcf578891264152de45113749bd96bb285cf
Guilherme França <guii_real@hotmail.com>
2020-01-15 01:16:44 -0300

    Do not include duplicate devs

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -6,22 +6,26 @@ module.exports = {
   async store(request, response) {
     const {github_username, techs, latitude, longitude} = request.body;
     const techsArray = techs.split(',').map(tech => tech.trim());
-    const {data: githubData} = await githubApi.get(`/users/${github_username}`);
-    const {name = login, avatar_url, bio} = githubData;
 
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude]
-    };
+    let dev = await Dev.findOne({github_username});
+    if (!dev) {
+      const {data: githubData} = await githubApi.get(`/users/${github_username}`);
+      const {name = login, avatar_url, bio} = githubData;
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
     return response.json(dev);
   }
 };
